e66b4c50ea52caacee838780a922c9d266349e99
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-07 18:24:02 +0100

    Update variables.tf

--- labs/lab-04-iam-service-accounts/variables.tf
+++ labs/lab-04-iam-service-accounts/variables.tf
@@ -1,1 +1,12 @@
+variable "project_id" { type = string }
+variable "region"     { type = string default = "europe-west2" }
 
+variable "tf_sa_name" {
+  type    = string
+  default = "tf-automation-sa"
+}
+
+variable "vm_sa_name" {
+  type    = string
+  default = "workload-vm-sa"
+}
