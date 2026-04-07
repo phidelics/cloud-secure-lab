4f9a1b959d78e6407a7a26cafbc6f057eab9648d
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-07 18:21:11 +0100

    Update outputs.tf

--- labs/lab-02-secure-networking/outputs.tf
+++ labs/lab-02-secure-networking/outputs.tf
@@ -1,1 +1,7 @@
+output "vpc_name" { value = google_compute_network.vpc.name }
+output "public_subnet" { value = google_compute_subnetwork.public.name }
+output "private_subnet" { value = google_compute_subnetwork.private.name }
 
+output "public_cidr" { value = google_compute_subnetwork.public.ip_cidr_range }
+output "private_cidr" { value = google_compute_subnetwork.private.ip_cidr_range }
+``
