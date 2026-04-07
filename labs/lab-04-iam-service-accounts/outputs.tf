a54bc2512065614d35819e5b05e508b9d0613e1e
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-07 18:24:48 +0100

    Update outputs.tf

--- labs/lab-04-iam-service-accounts/outputs.tf
+++ labs/lab-04-iam-service-accounts/outputs.tf
@@ -1,1 +1,8 @@
+output "tf_automation_sa_email" {
+  value = google_service_account.tf_automation.email
+}
+
+output "vm_workload_sa_email" {
+  value = google_service_account.vm_workload.email
+}
 
