bbae22d45ec2522765abc06081e473132accde24
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-07 18:26:12 +0100

    Update outputs.tf

--- labs/lab-05-secure-vm-deployment/outputs.tf
+++ labs/lab-05-secure-vm-deployment/outputs.tf
@@ -1,1 +1,11 @@
+output "vm_name" {
+  value = google_compute_instance.private_vm.name
+}
 
+output "vm_internal_ip" {
+  value = google_compute_instance.private_vm.network_interface[0].network_ip
+}
+
+output "vm_service_account" {
+  value = google_service_account.vm_workload.email
+}
