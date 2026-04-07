7b8ba438554fdd88823489c8032e1eed91b2a965
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-07 18:19:01 +0100

    Update outputs.tf

--- labs/lab-01-project-bootstrap/outputs.tf
+++ labs/lab-01-project-bootstrap/outputs.tf
@@ -1,1 +1,14 @@
+output "project_id" {
+  value       = var.project_id
+  description = "Project ID used for this lab"
+}
 
+output "vpc_name" {
+  value       = google_compute_network.vpc.name
+  description = "Created VPC name"
+}
+
+output "vpc_self_link" {
+  value       = google_compute_network.vpc.self_link
+  description = "VPC self_link"
+}
