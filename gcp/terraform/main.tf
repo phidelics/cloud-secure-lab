d28a3486b4a2e9c157c6f102e4e60f6f33457c26
phidelics <bamidelak4real@yahoo.com>
2026-04-07 10:57:37 +0100

    added terraform gcp setup

--- gcp/terraform/main.tf
+++ gcp/terraform/main.tf
@@ -1,1 +1,10 @@
 
+provider "google" {
+  project = "cloud-secure-lab"
+  region  = "europe-west2"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "secure-vpc"
+  auto_create_subnetworks = false
+}
