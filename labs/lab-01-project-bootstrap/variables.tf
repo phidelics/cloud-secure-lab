6ac8f26bd3f16ed716e237c9e2707f92a178624a
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-07 18:17:43 +0100

    Update variables.tf

--- labs/lab-01-project-bootstrap/variables.tf
+++ labs/lab-01-project-bootstrap/variables.tf
@@ -1,1 +1,16 @@
+variable "project_id" {
+  description = "GCP Project ID (NOT display name). Example: cloud-secure-lab-492609"
+  type        = string
+}
 
+variable "region" {
+  description = "Default region for resources"
+  type        = string
+  default     = "europe-west2"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC network"
+  type        = string
+  default     = "secure-vpc"
+}
