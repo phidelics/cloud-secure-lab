7294c874763af50227b7782e8687862124557d6e
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-09 18:07:26 +0100

    Update outputs.tf

--- labs/lab-02-secure-networking/outputs.tf
+++ labs/lab-02-secure-networking/outputs.tf
@@ -4,4 +4,4 @@ output "private_subnet" { value = google_compute_subnetwork.private.name }
 
 output "public_cidr" { value = google_compute_subnetwork.public.ip_cidr_range }
 output "private_cidr" { value = google_compute_subnetwork.private.ip_cidr_range }
-``
+
