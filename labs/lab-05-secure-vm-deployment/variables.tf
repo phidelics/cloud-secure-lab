71c4d815385910536424607f03c58167c8ac4787
Bamidele <96106630+phidelics@users.noreply.github.com>
2026-04-09 19:16:07 +0100

    Update variables.tf

--- labs/lab-05-secure-vm-deployment/variables.tf
+++ labs/lab-05-secure-vm-deployment/variables.tf
@@ -1,9 +1,21 @@
 variable "project_id" { type = string }
-variable "region"     { type = string default = "europe-west2" }
-variable "zone"       { type = string default = "europe-west2-a" }
+variable "region" { 
+  type = string 
+  default = "europe-west2"
+}
+variable "zone" { 
+  type = string 
+  default = "europe-west2-a"
+}
 
-variable "vpc_name"     { type = string default = "secure-vpc" }
-variable "private_cidr" { type = string default = "10.10.2.0/24" }
+variable "vpc_name" { 
+  type = string 
+  default = "secure-vpc"
+}
+variable "private_cidr" { 
+  type = string 
+  default = "10.10.2.0/24"
+}
 
 variable "vm_name" {
   type    = string
